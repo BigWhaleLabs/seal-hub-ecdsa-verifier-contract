f7e2050c41ad06cf4debd4cd4b5fabb161c2d7de
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-11-08 11:23:59 -0800

    add download public script

---
+++ scripts/download-public.sh
@@ -0,0 +1,3 @@
+curl https://bwl-zk.s3.amazonaws.com/ECDSAChecker.wasm -o public/ECDSAChecker.wasm
+curl https://bwl-zk.s3.amazonaws.com/ECDSAChecker_final.zkey -o public/ECDSAChecker_final.zkey
+curl https://bwl-zk.s3.amazonaws.com/ECDSAChecker_verification_key.json -o public/ECDSAChecker_verification_key.json
