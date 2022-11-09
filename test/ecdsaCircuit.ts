c6a39ac47d66c3f9f41946d235adf17f04a12b04
sarmat <skhachirov12@gmail.com>
2022-11-09 20:17:58 -0300

    fix wip

--- test/ecdsaCircuit.ts
+++ test/ecdsaCircuit.ts
@@ -4,21 +4,30 @@ import { wasm as wasmTester } from 'circom_tester'
 import Mimc7 from '../utils/Mimc7'
 import _ from 'lodash'
 import getECDSAInputs from '../utils/inputs/getECDSAInputs'
+import wallet from '../utils/wallet'
 
 describe('ECDSAChecker circuit', function () {
   before(async function () {
     this.circuit = await wasmTester('circuits/ECDSAChecker.circom')
     this.baseInputs = await getECDSAInputs()
   })
-  it('should generate the witness successfully and return correct mimc7', async function () {
+  it.only('should generate the witness successfully and return correct mimc7', async function () {
     const witness = await this.circuit.calculateWitness(this.baseInputs)
     await this.circuit.assertOut(witness, {})
 
-    const inputs = [
-      ..._.flattenDeep(this.baseInputs.TPreComputes),
-      ..._.flattenDeep(this.baseInputs.U),
-      ..._.flattenDeep(this.baseInputs.s),
-    ].map((v) => BigNumber.from(v))
+    const k = 4
+    const prepHash: number[] = []
+
+    for (let i = 0; i < k; i++) {
+      prepHash[i] = this.baseInputs.s[i]
+      prepHash[k + i] = this.baseInputs.U[0][i]
+      prepHash[i] = this.baseInputs.s[i]
+      prepHash[2 * k + i] = this.baseInputs.U[1][i]
+      prepHash[3 * k + i] = wallet.publicKey[0][i] as never
+      prepHash[4 * k + i] = wallet.publicKey[1][i] as never
+    }
+
+    const inputs = [..._.flattenDeep(prepHash)].map((v) => BigNumber.from(v))
 
     const mimc7 = await new Mimc7().prepare()
     const hash = mimc7.hash(inputs)
