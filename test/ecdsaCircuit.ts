11dbb6562437a6f6b0548ce4d7aa3fbde29987ce
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-12-08 17:26:19 -0800

    simplify the code with seal hub kit

--- test/ecdsaCircuit.ts
+++ test/ecdsaCircuit.ts
@@ -1,37 +1,28 @@
 import { Wallet } from 'ethers'
 import { expect } from 'chai'
+import {
+  getCommitmentFromSignature,
+  getMessageForAddress,
+} from '@big-whale-labs/seal-hub-kit'
 import { wasm as wasmTester } from 'circom_tester'
-import Mimc7 from '../utils/Mimc7'
-import buildInputs from '../utils/buildInputs'
 import getECDSAInputs from '../utils/inputs/getECDSAInputs'
 
 describe('ECDSAChecker circuit', function () {
   before(async function () {
     this.circuit = await wasmTester('circuits/ECDSAChecker.circom')
     this.wallet = Wallet.createRandom()
+    this.message = getMessageForAddress(this.wallet.address)
+    this.signature = await this.wallet.signMessage(this.message)
+    this.commitment = await getCommitmentFromSignature(
+      this.signature,
+      this.message
+    )
     this.baseInputs = await getECDSAInputs(this.wallet)
   })
-  it('should generate the witness successfully and return correct mimc7', async function () {
+  it('should generate the witness successfully and return correct commitment', async function () {
     const witness = await this.circuit.calculateWitness(this.baseInputs)
     await this.circuit.assertOut(witness, {})
-
-    const inputs = buildInputs(this.baseInputs, this.wallet)
-
-    const mimc7 = await new Mimc7().prepare()
-    const hash = mimc7.hash(inputs)
-    expect(hash).to.equal(witness[1])
-  })
-  it('should fail because inputs are different', async function () {
-    const witness = await this.circuit.calculateWitness(this.baseInputs)
-    await this.circuit.assertOut(witness, {})
-
-    const inputs = buildInputs(this.baseInputs, this.wallet)
-
-    // corrupt input
-    inputs[0] = BigInt(0)
-
-    const mimc7 = await new Mimc7().prepare()
-    const hash = mimc7.hash(inputs)
-    expect(hash).not.to.equal(witness[1])
+    // Check commitment
+    expect(this.commitment).to.equal(witness[1])
   })
 })
--- utils/inputs/getECDSAInputs.ts
+++ utils/inputs/getECDSAInputs.ts
@@ -1,104 +1,11 @@
-import { Wallet, utils } from 'ethers'
-import { hashPersonalMessage } from '@ethereumjs/util'
-import BN from 'bn.js'
-import elliptic from 'elliptic'
+import {
+  getMessageForAddress,
+  getSealHubInputs,
+} from '@big-whale-labs/seal-hub-kit'
 import wallet from '../wallet'
 
-const ec = new elliptic.ec('secp256k1')
-const STRIDE = 8n
-const NUM_STRIDES = 256n / STRIDE // = 32
-const REGISTERS = 4n
-
-const SECP256K1_N = new BN(
-  'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141',
-  16
-)
-
-const addHexPrefix = (str: string) => `0x${str}`
-
-interface ExtendedBasePoint extends elliptic.curve.base.BasePoint {
-  x: BN
-  y: BN
-}
-
-const splitToRegisters = (value?: BN | string) => {
-  const registers = [] as bigint[]
-
-  if (!value) {
-    return [0n, 0n, 0n, 0n]
-  }
-  const hex = value.toString('hex').padStart(64, '0')
-  for (let k = 0; k < REGISTERS; k++) {
-    // 64bit = 16 chars in hex
-    const val = hex.slice(k * 16, (k + 1) * 16)
-
-    registers.unshift(BigInt(addHexPrefix(val)))
-  }
-
-  return registers.map((el) => el.toString())
-}
-
-const getPointPreComputes = (point: ExtendedBasePoint) => {
-  const keyPoint = ec.keyFromPublic({
-    x: Buffer.from(point.x.toString(16), 'hex').toString('hex'),
-    y: Buffer.from(point.y.toString(16), 'hex').toString('hex'),
-  })
-
-  const gPowers = [] as (bigint[] | string[])[][][]
-  for (let i = 0n; i < NUM_STRIDES; i++) {
-    const stride: (bigint[] | string[])[][] = []
-    const power = 2n ** (i * STRIDE)
-    for (let j = 0n; j < 2n ** STRIDE; j++) {
-      const l = j * power
-
-      const gPower = keyPoint
-        .getPublic()
-        .mul(new BN(l.toString())) as ExtendedBasePoint
-      const x = splitToRegisters(gPower.x)
-      const y = splitToRegisters(gPower.y)
-      stride.push([x, y])
-    }
-    gPowers.push(stride)
-  }
-
-  return gPowers
-}
-
-async function inputsForMessage(signer: Wallet, message: string) {
-  const msgHash = hashPersonalMessage(Buffer.from(message))
+export default async function (signer = wallet) {
+  const message = getMessageForAddress(signer.address)
   const signature = await signer.signMessage(message)
-  const { v, r, s } = utils.splitSignature(signature)
-
-  const biV = BigInt(v)
-  const biR = new BN(r.slice(2, r.length), 'hex')
-  const hexS = s.slice(2, s.length)
-
-  const isYOdd = (biV - BigInt(27)) % BigInt(2)
-  const rPoint = ec.keyFromPublic(
-    ec.curve.pointFromX(new BN(biR), isYOdd).encode('hex'),
-    'hex'
-  )
-
-  // Get the group element: -(m * r^−1 * G)
-  const rInv = new BN(biR).invm(SECP256K1_N)
-
-  // w = -(r^-1 * msg)
-  const w = rInv.mul(new BN(msgHash)).neg().umod(SECP256K1_N)
-  // U = -(w * G) = -(r^-1 * msg * G)
-  const U = ec.curve.g.mul(w)
-
-  // T = r^-1 * R
-  const T = rPoint.getPublic().mul(rInv) as ExtendedBasePoint
-
-  const TPreComputes = getPointPreComputes(T)
-
-  return {
-    TPreComputes,
-    U: [splitToRegisters(U.x), splitToRegisters(U.y)],
-    s: [splitToRegisters(hexS)],
-  }
-}
-
-export default function (signer = wallet) {
-  return inputsForMessage(signer, 'Signature for SealHub')
+  return getSealHubInputs(signature, message)
 }
